a38f033c4c07284082be30442a8b568fded72f9e
TYRAlex <724172181@qq.com>
2021-10-18 16:11:32 +0800

    提交数据结构研究

---
+++ 数据结构研究/数据结构研究/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace 数据结构研究
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<int> linkedList=new LinkedList<int>();
+            linkedList.AddFirst(123);
+            linkedList.AddLast(456);
+
+            bool isContain = linkedList.Contains(123);
+            LinkedListNode<int> node123 = linkedList.Find(123);//元素123的位置，从头查找
+            linkedList.AddBefore(node123, 123);//节点不存在，会报错
+           // linkedList.add
+        }
+    }
+}
