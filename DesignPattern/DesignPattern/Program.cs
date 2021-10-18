c2c70624a265f7993676aa15a5b2a2660cee0537
TYRAlex <724172181@qq.com>
2021-10-18 15:16:49 +0800

    又来尝试修改了

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace DesignPattern
+﻿using System;
+
+namespace DesignPattern
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("这次又尝试修改一下111111111111111");
+            Console.ReadKey();
         }
     }
 }
