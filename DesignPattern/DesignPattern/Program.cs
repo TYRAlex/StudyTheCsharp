484885bd94c6c4ba92e34b51f51b81e8de816014
TYRAlex <724172181@qq.com>
2021-10-18 15:01:22 +0800

    这是是真的添加观察者模式代码

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace DesignPattern
+﻿using System;
+
+namespace DesignPattern
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("测试上传方法");
+            Console.ReadKey();
         }
     }
 }
