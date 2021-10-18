7c5671aadc4c61ed8ec19fdf253540253b8241ec
TYRAlex <724172181@qq.com>
2021-10-18 15:14:55 +0800

    这次尝试修改一下

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -1,13 +1,9 @@
-﻿using System;
-
-namespace DesignPattern
+﻿namespace DesignPattern
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("测试上传方法");
-            Console.ReadKey();
         }
     }
 }
