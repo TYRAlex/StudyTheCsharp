c9e72ed8b24e32cb2674b376c36c34d5bc869d61
TYRAlex <724172181@qq.com>
2021-10-18 13:48:09 +0800

    测试上传

---
+++ LeecodeStudy/LeecodeStudy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeecodeStudy
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("这是一个测试方法");
+            Console.ReadKey();
+        }
+    }
+}
