b1fbae9881b96f4e62c6370e14d75271e62200c5
TYRAlex <724172181@qq.com>
2021-11-03 14:01:18 +0800

    修改测试上传

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -7,6 +7,9 @@ namespace DesignPattern
         public static void Main(string[] args)
         {
             Console.WriteLine("这次又尝试修改一下111111111111111");
+            
+            Console.WriteLine(0.53345/1);
+            
             Console.ReadKey();
         }
     }
